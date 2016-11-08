8d624ff833e30e23bc617912bce31fdeafa4cc1b
Jackson Hoose <jacksonhoose@gmail.com>
2016-11-07 17:22:24 -0800

    first commit

---
+++ web/router.ex
@@ -0,0 +1,32 @@
+defmodule Discuss.Router do
+  use Discuss.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Discuss do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    get "/topics/", TopicController, :index
+    get "/topics/new", TopicController, :new
+    # get "/topics/:id", TopicController, :show
+    # post "/topics/new", TopicController, :create
+    # delete "/topics/:id", TopicController, :delete
+
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Discuss do
+  #   pipe_through :api
+  # end
+end
